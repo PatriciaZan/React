f5ba42fdd88cb69592fe86fbcbb907b8f23e0c62
PatriciaZan <patricia.zan.oliveira@gmail.com>
2025-01-24 17:53:12 -0300

    first commit

---
+++ Beginners Projects/src/App.jsx
@@ -0,0 +1,20 @@
+// import Counter from "./components/Counter"
+//import Todo from "./components/Todo"
+//import Meals from "./components/Meals"
+//import Calculator from "./components/Calculator"
+
+import ToggleBackgroundColor from "./components/ToggleBackgroundColor"
+
+const App = () => {
+  return (
+    <div>
+      {/* <Counter /> */}
+      {/* <Todo /> */}
+      {/* <Meals /> */}
+      {/* <Calculator /> */}
+      <ToggleBackgroundColor />
+    </div>
+  )
+}
+
+export default App
