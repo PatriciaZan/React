bb5a445d4e86761b9346737c395d8eb5227f4b7b
PatriciaZan <patricia.zan.oliveira@gmail.com>
2025-03-14 14:51:18 -0300

    animation

--- 3FrameMotion/src/App.tsx
+++ 3FrameMotion/src/App.tsx
@@ -1,12 +1,30 @@
-import {motion} from 'framer-motion'
+//import {motion} from 'framer-motion'
+//import BouncingLoader from "./components/BouncingLoader/BouncingLoader"
+
+import FlippingCard from "./components/FlippingCard/FlippingCard"
 
 const App = () => {
   return (
     <div>
-      <motion.div 
+      {/* <motion.div 
         className='box'
+        // animate
         // translateX: '200px' = x-200
-        animate={{ x:200 }}/> 
+        // animate={{ x:200 }}/> 
+
+        // scale | skew
+        // animate={{ scale: 2 }}/>  
+
+        // Transition
+        initial={{ x:0}} 
+        animate={{ x: 100 }} 
+        // transition={{ delay: 2 }}
+        transition={{ duration: 2, ease: 'easeInOut'}}
+         />   */}
+        
+      {/* <BouncingLoader/> */}
+      <FlippingCard />
+
     </div>
   )
 }
