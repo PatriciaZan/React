6907914303f987446fa34c115b31302a75074d13
PatriciaZan <patricia.zan.oliveira@gmail.com>
2025-04-08 14:15:33 -0300

    save files

--- 3FrameMotion/src/components/StaggerAnimation/StaggerAnimation.tsx
+++ 3FrameMotion/src/components/StaggerAnimation/StaggerAnimation.tsx
@@ -1,11 +1,20 @@
-import { motion } from "framer-motion"
+import { motion, stagger } from "framer-motion"
 
+const parentVar = {
+    hidden: {oapcity: 0},
+    visible: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.8,
+        }
+    }
+}
 
 const StaggerAnimation = () => {
   return (
     <div>
         {[...Array(5)].map((_, index) => (
-            <div className="box mt-[2rem]"></div>
+            <div className="box mt-[2rem]" key={index}></div>
         ))}
     </div>
   )
