3514758d336d7ac8a517ffd4b1a5f29190fe9706
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-04-21 14:54:44 -0300

    Scrimba Course

---
+++ 5Scrimba/src/components/Entry.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Entry = () => {
+  return (
+    <article>
+        <img src="../assets/mountFuji.pnj" alt="Mount Fuji" />
+
+        <div>
+            {/* <img  alt="" /> */}
+            <span>Japan</span>
+            <a href="">View on Google Maps</a>
+        </div>
+    </article>
+  )
+}
+
+export default Entry
--- 5Scrimba/src/components/Header.jsx
+++ 5Scrimba/src/components/Header.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        <h1>My Travel Journal</h1>
+    </header>
   )
 }
 
