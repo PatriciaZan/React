6e8a84f6bd9758e3159cb774efaff50464835496
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-04-21 14:24:18 -0300

    Scrimba Course

---
+++ 5Scrimba/src/data/data.js
@@ -0,0 +1,41 @@
+
+export default [
+    {
+        id: 1,
+        img: {
+            src: "../assets/mountFuji.jpg",
+            alt: "Mount Fuji"
+        },
+        title: "Mont Fuji",
+        country: "Japan",
+        googleMapsLink: "https://maps.app.goo.gl/fgyJDFi2wSpSzp4i7",
+        dates: "10 Jan, 2025 - 26 Jan, 2025",
+        text: "Mount Fuji, highest mountain in Japan. It rises to 12,388 feet (3,776 metres) near the Pacific Ocean coast in Yamanashi and Shizuoka ken (prefectures) of central Honshu, about 60 miles (100 km) west of the Tokyo-Yokohama metropolitan area."
+    },
+
+    {
+        id: 2,
+        img: {
+            src: "../assets/torontocanada.jpg",
+            alt: "Toronto Canada"
+        },
+        title: "Toronto City",
+        country: "Canada",
+        googleMapsLink: "https://maps.app.goo.gl/AZagcNUqNoEw64dn7",
+        dates: "04 May, 2019 - 21 May, 2019",
+        text: "Toronto is the most populous city in Canada and the capital city of the Canadian province of Ontario. With a population of 2,794,356 in 2021,[9] it is the fourth-most populous city in North America."
+    },
+
+    {
+        id: 3,
+        img: {
+            src: "../assets/spaceneedle.jpg",
+            alt: "Space Needle"
+        },
+        title: "Space Needle",
+        country: "Seattle",
+        googleMapsLink: "https://maps.app.goo.gl/HUqgd2qUGo9CL8vm6",
+        dates: "15 Sep, 2024 - 28 Sep, 2024",
+        text: "At 605 feet tall, the landmark was constructed to symbolize humanity’s Space Age aspirations, and since its grand opening on April 21, 1962, continues to represent the innovative and forward-thinking spirit of Seattle."
+    },
+]
