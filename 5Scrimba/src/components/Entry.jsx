37c8d5376d59751cc9629dcc4b7eaf1ba87b4a94
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-04-22 15:18:51 -0300

    Project 1

--- 5Scrimba/src/components/Entry.jsx
+++ 5Scrimba/src/components/Entry.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 const Entry = () => {
   return (
-    <article>
-        <img src="../assets/mountFuji.pnj" alt="Mount Fuji" />
+    <article className='journal-entry'>
+      <div>
+        <img className='main-image' src="src/assets/mountFuji.jpg" alt="Mount Fuji" />
+      </div>
 
-        <div>
+        <div className='main-container'>
             {/* <img  alt="" /> */}
-            <span>Japan</span>
+            <span className='span-country'>Japan</span>
             <a href="">View on Google Maps</a>
+            <h2 className='h2-title'>Mount Fuji</h2>
+            <p>10 Jan, 2025 - 26 Jan, 2025</p>
+            <p className='p-about'>Mount Fuji, highest mountain in Japan. It rises to 12,388 feet (3,776 metres) near the Pacific Ocean coast in Yamanashi and Shizuoka ken (prefectures) of central Honshu, about 60 miles (100 km) west of the Tokyo-Yokohama metropolitan area.</p>
         </div>
     </article>
   )
