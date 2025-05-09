1493446d635d663d5c98cff397adcd35b3cd009b
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-05-09 16:17:15 -0300

    Trying to fix bug of undefined

--- 5Scrimba/Assembly/src/App.jsx
+++ 5Scrimba/Assembly/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
 import './App.css'
-import {languages} from './languages/languages'
+import { useState } from 'react'
+import { languages } from './languages/languages'
+import { clsx } from  'clsx'
 
 
 function Assembly() {
-  const [currentWord, setCurrentWord] = useState("react")
-
+  const [currentWord, setCurrentWord] = useState("react");
+  const [guessedLetters, setGuessedLetters] = useState([])
+  //console.log(guessedLetters);
+  
   const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+  function addGueseedLetter(letter){
+    setGuessedLetters(prevLetters => {
+     prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter]
+
+     // Another way of soing it, but it re-renders all the component, not good
+      // const letterSet = new Set(prevLetters)
+      // letterSet.add(letter)
+      // return Array.from(letterSet)
+    })
+  }
 
   const languageElements = languages.map(lang => {
     const styles = {
@@ -22,13 +36,39 @@ function Assembly() {
     )
   })
 
-  const letterElements = [...currentWord].map((letter,index) => (
+  const letterElements = currentWord.split("").map((letter,index) => (
     <span key={index}>{letter.toUpperCase()}</span>
   ))
 
-  const keyboardElements = [...alphabet].map(letter => (
-    <button key={letter}>{letter.toUpperCase()}</button>
-  ))
+//  const keyboardElements = alphabet.split("").map(letter => {
+//         const isGuessed = guessedLetters.includes(letter)
+//         const isCorrect = isGuessed && currentWord.includes(letter)
+//         const isWrong = isGuessed && !currentWord.includes(letter)
+//         const className = clsx({
+//             correct: isCorrect,
+//             wrong: isWrong
+//         })
+
+    //console.log(className);
+    const keyboardElements = alphabet.split("").map(letter => (
+        <button
+            key={letter}
+            onClick={() => addGuessedLetter(letter)}
+        >
+            {letter.toUpperCase()}
+        </button>
+    ))
+    
+    
+
+   return (
+     <button 
+        className={className}
+        key={letter} 
+        onClick={() => addGueseedLetter(letter)}
+      >{letter.toUpperCase()}</button>
+   )}
+  )
   
 
   return (
