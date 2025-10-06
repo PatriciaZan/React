008f7afe2ea3dd3bb9c5a39669f45d92449f000e
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-10-06 15:03:15 -0300

    [CLASS + CHALLENGE] Lifting state | Tip calculator challenge

---
+++ 6-Udemy-TheUltimateReactCourse/Challenges/base-challenges/src/App.jsx
@@ -0,0 +1,14 @@
+// HERE ALL THE CHALLENGES WILL BE IMPORTED
+
+import TipCalculator from "../../tip-calculator/TipCalculator";
+
+// To see a specific challenge please uncomente the import and component
+function App() {
+  return (
+    <>
+      <TipCalculator />
+    </>
+  );
+}
+
+export default App;
