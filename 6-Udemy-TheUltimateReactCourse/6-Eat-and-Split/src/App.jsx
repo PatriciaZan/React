584f6229a8d6eae8e30045adb6b22a000be10351
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-10-13 20:42:51 -0300

    [LEARNING] async React components | React DOM | How does it work

--- 6-Udemy-TheUltimateReactCourse/6-Eat-and-Split/src/App.jsx
+++ 6-Udemy-TheUltimateReactCourse/6-Eat-and-Split/src/App.jsx
@@ -72,6 +72,7 @@ export default function App() {
 
       {selectedFriend && (
         <FormSplitBill
+          key={selectedFriend.id}
           selectFriend={selectedFriend}
           onSplitBill={handleSplitBill}
         />
