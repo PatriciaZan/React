8fb949ec9085711cee59cbccfa58484524d79474
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-10-14 19:24:30 -0300

    [LEARNING] useEffects async functions | popCorn project error handling

--- 6-Udemy-TheUltimateReactCourse/7-use-Pop-Corn/src/main.jsx
+++ 6-Udemy-TheUltimateReactCourse/7-use-Pop-Corn/src/main.jsx
@@ -1,17 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-//import "./index.css";
-//import App from './App.jsx'
-import StartRating from "./components/StartRating.jsx";
+import "./index.css";
+import App from "./App.jsx";
+//import StartRating from "./components/StartRating.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    {/* <App /> */}
-    <StartRating
-      maxRating={10}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-      defaultRating={3}
-      // onSetRating={setMovieRating}
-    />
-  </StrictMode>
+  <>
+    <App />
+  </>
 );
