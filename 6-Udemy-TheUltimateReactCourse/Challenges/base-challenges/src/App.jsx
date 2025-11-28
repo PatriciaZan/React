48ba3363da18988771977e85499ffc3409a94e5a
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-11-28 15:54:09 -0300

    [CHALLENGE] 5 -> Currency converter using an API

--- 6-Udemy-TheUltimateReactCourse/Challenges/base-challenges/src/App.jsx
+++ 6-Udemy-TheUltimateReactCourse/Challenges/base-challenges/src/App.jsx
@@ -1,12 +1,14 @@
 // HERE ALL THE CHALLENGES WILL BE IMPORTED
 
 import TextExpanderApp from "../../4-text-expander/TextExpander";
+import CurrencyConverter from "../../5-currency-converter/CurrencyConverter";
 
 // To see a specific challenge please uncomente the import and component
 function App() {
   return (
     <>
-      <TextExpanderApp />
+      {/* <TextExpanderApp /> */}
+      <CurrencyConverter />
     </>
   );
 }
