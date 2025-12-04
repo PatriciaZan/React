474de1abba83c69ece044247229393959ab64c58
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-12-04 14:47:42 -0300

    [CHALLENGE] useRef on a simple get location app

--- 6-Udemy-TheUltimateReactCourse/Challenges/base-challenges/src/App.jsx
+++ 6-Udemy-TheUltimateReactCourse/Challenges/base-challenges/src/App.jsx
@@ -2,13 +2,14 @@
 
 import TextExpanderApp from "../../4-text-expander/TextExpander";
 import CurrencyConverter from "../../5-currency-converter/CurrencyConverter";
-
+import Geolocate from "../../6-useGeolocate/Geolocate";
 // To see a specific challenge please uncomente the import and component
 function App() {
   return (
     <>
       {/* <TextExpanderApp /> */}
-      <CurrencyConverter />
+      {/* <CurrencyConverter /> */}
+      <Geolocate />
     </>
   );
 }
