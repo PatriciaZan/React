a18ccbd19d3f6ad08505dde90dc791833cfee030
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-12-12 15:39:31 -0300

    [CHALLENGE] Bank account using useReducer

--- 6-Udemy-TheUltimateReactCourse/Challenges/base-challenges/src/App.jsx
+++ 6-Udemy-TheUltimateReactCourse/Challenges/base-challenges/src/App.jsx
@@ -3,13 +3,15 @@
 import TextExpanderApp from "../../4-text-expander/TextExpander";
 import CurrencyConverter from "../../5-currency-converter/CurrencyConverter";
 import Geolocate from "../../6-useGeolocate/Geolocate";
-// To see a specific challenge please uncomente the import and component
+import BankAccount from "../../7-bank-account/BankAccount";
+// To see a specific challenge please uncomment the import and component
 function App() {
   return (
     <>
       {/* <TextExpanderApp /> */}
       {/* <CurrencyConverter /> */}
-      <Geolocate />
+      {/* <Geolocate /> */}
+      <BankAccount />
     </>
   );
 }
