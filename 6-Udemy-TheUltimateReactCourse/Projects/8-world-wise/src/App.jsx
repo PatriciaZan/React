855e369a3095db36cbf884eaf6c2fe8f441f5c59
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-12-25 19:46:06 -0300

    [FEAT + STUDY] Dynamic routing

--- 6-Udemy-TheUltimateReactCourse/Projects/8-world-wise/src/App.jsx
+++ 6-Udemy-TheUltimateReactCourse/Projects/8-world-wise/src/App.jsx
@@ -8,6 +8,8 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -47,7 +49,11 @@ export default function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
--- 6-Udemy-TheUltimateReactCourse/Projects/8-world-wise/src/components/Map.jsx
+++ 6-Udemy-TheUltimateReactCourse/Projects/8-world-wise/src/components/Map.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 import styles from "./Map.module.css";
+import { useSearchParams } from "react-router-dom";
 
 export default function Map() {
+  const { searchParams, setSearch } = useSearchParams();
   return <div className={styles.mapContainer}>Map</div>;
 }
