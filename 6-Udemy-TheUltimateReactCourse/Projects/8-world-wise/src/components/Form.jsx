34f5e873e4864269e4c52c24e114fd0c5eea3cf6
Patricia Zan <patricia.zan.oliveira@gmail.com>
2026-01-09 18:59:36 -0300

    [BUGFIX] WorldWise -> Fix when click on map to show location

--- 6-Udemy-TheUltimateReactCourse/Projects/8-world-wise/src/components/Form.jsx
+++ 6-Udemy-TheUltimateReactCourse/Projects/8-world-wise/src/components/Form.jsx
@@ -1,11 +1,12 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "./Form.module.css";
 import Button from "./Button";
 import BackButton from "./BackButton";
 import { useNavigate } from "react-router-dom";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -15,13 +16,42 @@ export function convertToEmoji(countryCode) {
   return String.fromCodePoint(...codePoints);
 }
 
+const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
+
 function Form() {
   const navigate = useNavigate();
+
+  const [lat, lng] = useUrlPosition();
 
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
+
+  const [isLoadingGiocoding, setIsLoadingGiocoding] = useState(false);
+
+  useEffect(
+    function () {
+      async function fetchCityData() {
+        try {
+          setIsLoadingGiocoding(true);
+          const res = await fetch(
+            `${BASE_URL}?latitude=${lat}&longitude=${lng}`
+          );
+          const data = await res.json();
+          console.log(data);
+          setCityName(data.city || data.locality || "");
+          setCountry(data.countryName);
+        } catch (err) {
+          console.log("ERROR: ", err);
+        } finally {
+          setIsLoadingGiocoding(false);
+        }
+      }
+      fetchCityData();
+    },
+    [lat, lng]
+  );
 
   return (
     <form className={styles.form}>
